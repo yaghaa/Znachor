c51306d454aabe058759e3b102b9542da6c90b53
kopawow <ciekawy.kopa@gmail.com>
2017-06-10 12:55:23 +0200

    dodany kontroler, obsługa walidacjio nowy widok

--- Znachor/Controllers/OrderController.cs
+++ Znachor/Controllers/OrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Znachor.ViewModels;
 
 namespace Znachor.Controllers
 {
@@ -14,5 +15,17 @@ namespace Znachor.Controllers
             var model = new Znachor.ViewModels.OrderViewModel();
             return View(model);
         }
+
+      [HttpPost]
+      public ActionResult Index(OrderViewModel model)
+      {
+      if(!ModelState.IsValid)
+        {
+        return View(model);
+      }
+       
+      //TODO: Zapis do bazy
+      return RedirectToAction("Index", "Payment");
+    }
     }
 }
--- Znachor/ViewModels/OrderViewModel.cs
+++ Znachor/ViewModels/OrderViewModel.cs
@@ -8,16 +8,23 @@ namespace Znachor.ViewModels
 {
   public class OrderViewModel
   {
+    [Required]
     public string Shipment  { get; set; }
+    [Required]
     public string Payment { get; set; }
     [Display(Name = "Imię")]
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     [Display(Name = "Nazwisko")]
     public string Surname { get; set; }
+    [Required]
     [Display(Name = "Ulica")]
     public string Street { get; set; }
+    [Required]
     [Display(Name = "Kod pocztowy")]
     public string PostalCode { get; set; }
+    [Required]
     [Display(Name = "Miasto")]
     public string City { get; set; }
   }
