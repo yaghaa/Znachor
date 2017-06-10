b676ef9c42bfddc0df80755f138df062c9cd1ee5
yaghaa <za.rogiem@gmail.com>
2017-06-10 14:45:59 +0200

    cos, zapis nie dziala

--- Znachor/Controllers/OrderController.cs
+++ Znachor/Controllers/OrderController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using Znachor.ViewModels;
 
 namespace Znachor.Controllers
 {
     public class OrderController : Controller
     {
-        // GET: Order
-        public ActionResult Index()
+      private Models.Znachor ctx = new Models.Znachor();
+
+    // GET: Order
+    public ActionResult Index()
         {
             var model = new Znachor.ViewModels.OrderViewModel();
             return View(model);
@@ -23,8 +26,30 @@ namespace Znachor.Controllers
         {
         return View(model);
       }
-       
-      //TODO: Zapis do bazy
+        var userId = User.Identity.GetUserId();
+        //var cartContent = ctx.Koszyks.Where(x => x.AspNetUsersid == userId);
+      var zamowienie = new Znachor.Models.Zamowienie
+      {
+        Klientid_klienta = userId,
+        Platnoscrodzaj = model.Payment,
+        Przesylkaid_przesylki = 0,
+        nr_rachunku = "123",
+      };
+        ctx.Zamowienies.Add(zamowienie);
+        //ctx.SaveChanges();
+
+        //var zamowienieId = ctx.Zamowienies.FirstOrDefault(x => x.Klientid_klienta == userId);
+        var address = new Znachor.Models.Adresprzesylki
+        {
+          ulica = model.Street,
+          kodpocztowy = model.PostalCode,
+          miejscowosc = model.City,
+          nrdomu = 1,
+        };
+        ctx.Adresprzesylkis.Add(address);
+        ctx.SaveChanges();
+
+        //TODO: Zapis do bazy
       return RedirectToAction("Index", "OderDetails");
      }
     }
